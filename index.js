b2cd5c00dbf6b24c0553e01f31d313631893c14a
git config --global user.name John <martinsavov@ymail.com>
2020-07-31 14:48:38 +0300

    enter

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import ObjectDataPreviewModal from "./modalObject.js";
 import ObjectDataPreviewTable from "./tableObject.js";
 
 const initTable = props =>{
-    new ObjectDataPreviewTable(props)
+    new ObjectDataPreviewTable(props);
 }
 
 const app = async () => {
     fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json)
+        .then(response => response.json())
         .then(data => {
             initTable({
                 data,
